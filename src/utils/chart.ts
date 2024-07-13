d58a8c8af0ae1ca28f7e1f7730cda2a2cb877697
Gregory Danielson <gregory.danielson3@gmail.com>
2024-07-12 22:30:07 -0500

    fix: missing row import is back

--- src/utils/chart.ts
+++ src/utils/chart.ts
@@ -1,5 +1,5 @@
 import { htmlLegendPlugin, verticalLinePlugin } from "@utils/plugins";
-import type { Result } from "@utils/search";
+import type { Result, Row } from "@utils/search";
 import type { ChartTypeRegistry, TooltipItem } from "chart.js/auto";
 import Chart from "chart.js/auto";
 import "chartjs-adapter-date-fns";
