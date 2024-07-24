2a606b2adcd680cd2a3a9c8ef6125f733c1bb3f0
Gregory Danielson <gregory.danielson3@gmail.com>
2024-07-23 22:09:28 -0500

    fix: ensure only one db is constructed
    promises are weird

--- src/utils/sqlite.ts
+++ src/utils/sqlite.ts
@@ -2,7 +2,7 @@ import { createDbWorker } from "sql.js-httpvfs";
 import type { WorkerHttpvfs } from "sql.js-httpvfs";
 import { BASE_URL, DB_URL } from "@utils/constants";
 
-let worker: WorkerHttpvfs | null = null;
+let workerPromise: Promise<WorkerHttpvfs> | null = null;
 
 export async function initDB(dbUrl: string): Promise<WorkerHttpvfs> {
   const worker = await createDbWorker(
@@ -25,24 +25,10 @@ export async function initDB(dbUrl: string): Promise<WorkerHttpvfs> {
 
 /* TODO: queryresult? */
 export async function queryDb(query: string, params: any[]): Promise<any[]> {
-  if (!worker) {
-    worker = await initDB(DB_URL);
+  if (!workerPromise) {
+    workerPromise = initDB(DB_URL);
   }
 
+  const worker = await workerPromise;
   return await worker.db.query(query, params);
 }
-
-//
-// const remoteURL =
-//   'https://velivole.b-cdn.net/maptiler-osm-2017-07-03-v3.6.1-europe.mbtiles';
-// const pool = await createSQLiteHTTPPool({ workers: 8 });
-// await pool.open(remoteURL);
-// // This will automatically use a free thread from the pool
-// const tile = await pool.exec(
-//     'SELECT zoom_level, tile_column, tile_row, tile_data FROM tiles ' +
-//     'WHERE zoom_level = 10 AND tile_column = $col AND tile_row = $row',
-//     { $col: 600, $row: 600 });
-// console.log(tile[0].columnNames);
-// console.log(tile[0].row);
-// // This shutdowns the pool
-// await pool.close();
