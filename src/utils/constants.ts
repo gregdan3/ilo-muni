757b20138d22ae27dbac429c188e15374ef0281e
Gregory Danielson <gregory.danielson3@gmail.com>
2024-07-29 13:13:01 -0500

    chore: update db dump
    this should be the final one before publishing, unless i suddenly get
    Cracked and manage to import all the forum/fb data

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 import { makeAugust } from "@utils/other";
 
 export const BASE_URL = "/ilo-muni";
-export const DB_URL = `${BASE_URL}/db/2024-07-18-trimmed.sqlite`;
+export const DB_URL = `${BASE_URL}/db/2024-07-29-trimmed.sqlite`;
 export const PHRASE_RE =
   /^[a-zA-Z0-9\uF1900-\uF1977\uF1978-\uF1988\uF19A0-\uF19A3* ]+(?:_\d)?$/;
 
