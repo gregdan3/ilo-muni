60e181755d36303b0c8214ca270e1d4e669be93f
Gregory Danielson <gregory.danielson3@gmail.com>
2024-07-29 08:40:16 -0500

    fix: EARLIEST_YEAR should not go below 2015
    there is data to 2010, but almost none

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,8 +5,10 @@ export const DB_URL = `${BASE_URL}/db/2024-07-18-trimmed.sqlite`;
 export const PHRASE_RE =
   /^[a-zA-Z0-9\uF1900-\uF1977\uF1978-\uF1988\uF19A0-\uF19A3* ]+(?:_\d)?$/;
 
-// TODO: fetch from db at init
-export const EARLIEST_YEAR = 2009;
+// TODO: fetch from db at init? would make it not so much a constant
+export const EARLIEST_YEAR = 2015;
+// NOTE: the data goes back to 2010 but there is almost nothing there
+// export const EARLIEST_YEAR = 2010;
 export const LATEST_YEAR = 2024;
 
 export const EARLIEST_TIMESTAMP = makeAugust(EARLIEST_YEAR).getTime() / 1000;
