0462b6b4ce1612c58bce720ee9e2ca800a176996
Gregory Danielson <gregory.danielson3@gmail.com>
2024-08-03 22:37:44 -0500

    feat: countSubstring util

--- src/utils/other.ts
+++ src/utils/other.ts
@@ -7,3 +7,19 @@ export function makeAugust(year: number) {
 export function isUCSUR(s: string): boolean {
   return UCSUR_RE.test(s);
 }
+
+export function countSubstring(toSearch: string, substring: string): number {
+  if (substring.length === 0) {
+    return 0; // Edge case: empty substring should return 0
+  }
+
+  let count = 0;
+  let pos = toSearch.indexOf(substring);
+
+  while (pos !== -1) {
+    count++;
+    pos = toSearch.indexOf(substring, pos + substring.length);
+  }
+
+  return count;
+}
