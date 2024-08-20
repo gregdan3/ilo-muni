50e5f14cc9ac0a8a4d8f66beb77f367dd859707b
Gregory Danielson <gregory.danielson3@gmail.com>
2024-08-19 19:12:59 -0500

    chore: update astro with minor new deps
    Former-commit-id: 90de58f7b13e80ac62208bac6f41f6ccb086af23

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,29 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://gregdan3.github.io/ilo-muni/",
   base: "/ilo-muni",
   publicDir: "./static",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()],
   markdown: {
-    remarkPlugins: [[remarkToc, { heading: "Table of Contents", maxDepth: 3 }]],
+    remarkPlugins: [[remarkToc, {
+      heading: "Table of Contents",
+      maxDepth: 3
+    }]]
   },
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin",
-    },
+      "Cross-Origin-Opener-Policy": "same-origin"
+    }
   },
   devToolbar: {
-    enabled: false,
-  },
+    enabled: false
+  }
 });
