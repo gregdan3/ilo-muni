33545987207f1f14c2a303a2fea6ff7e962deb89
Gregory Danielson <gregory.danielson3@gmail.com>
2024-08-19 19:20:45 -0500

    chore: util function for copying url to clipboard
    it returns its result as a user readable string bc i intend to display
    that on a small notif
    
    
    Former-commit-id: cd1efa8608bd3a6fe79bf08a41789261c5580bc6

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -4,3 +4,13 @@ import { randomElem } from "@utils/other";
 export function randomQuery(): string {
   return randomElem(SAMPLE_SEARCHES);
 }
+
+export async function copyUrlToClipboard(): Promise<string> {
+  try {
+    const url = window.location.href;
+    await navigator.clipboard.writeText(url);
+    return "URL copied to clipboard!";
+  } catch (err) {
+    return `Failed to copy URL: ${err}`;
+  }
+}
