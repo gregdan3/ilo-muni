1754a3ec114200cc18edafb821f3a37cca676830
Gregory Danielson <gregory.danielson3@gmail.com>
2024-10-04 15:07:48 -0500

    o sitelen sin
    Former-commit-id: 543f3f6ce11080c6ee49181b6664ff51586a0b66

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
+import compress from "astro-compress";
+import robotsTxt from "astro-robots-txt";
 
 import icon from "astro-icon";
 
@@ -10,20 +12,31 @@ export default defineConfig({
   site: "https://gregdan3.github.io/ilo-muni/",
   base: "/ilo-muni",
   publicDir: "./static",
-  integrations: [mdx(), sitemap(), icon()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({ iconDir: "src/icons" }),
+    robotsTxt(),
+    compress(),
+  ],
   markdown: {
-    remarkPlugins: [[remarkToc, {
-      heading: "Table of Contents",
-      maxDepth: 3
-    }]]
+    remarkPlugins: [
+      [
+        remarkToc,
+        {
+          heading: "Table of Contents",
+          maxDepth: 3,
+        },
+      ],
+    ],
   },
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin"
-    }
+      "Cross-Origin-Opener-Policy": "same-origin",
+    },
   },
   devToolbar: {
-    enabled: false
-  }
+    enabled: false,
+  },
 });
