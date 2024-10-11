df446d2cfa5a4b634e6679802017e66bb734a94a
Gregory Danielson <gregory.danielson3@gmail.com>
2024-10-11 15:58:16 -0500

    feat: count the entire url on each search (closes #8)
    Former-commit-id: e40d8397715aba307bb0a342aa49b2a90d4fdc7e

---
+++ src/utils/analytics.ts
@@ -0,0 +1,31 @@
+export function countQuery(
+  maxAttempts: number = 4,
+  interval: number = 250,
+): boolean {
+  const path = location.pathname + location.search + location.hash;
+  let attempts = 0;
+  let counted = false;
+
+  function count() {
+    // @ts-expect-error: it will exist
+    if (window.goatcounter && window.goatcounter.count) {
+      clearInterval(t);
+      console.log(`Counting ${path}`);
+      // @ts-expect-error: it does exist
+      window.goatcounter.count({
+        path: path,
+        title: document.title,
+        referrer: document.referrer,
+      });
+      counted = true;
+    } else if (attempts >= maxAttempts) {
+      clearInterval(t);
+    }
+  }
+
+  const t = setInterval(function () {
+    attempts++;
+    count();
+  }, interval);
+  return counted;
+}
